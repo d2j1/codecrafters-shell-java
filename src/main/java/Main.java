34e51b742f116cada1a4d89db1a1753b19fcd4c3
d2j1 <dhananjayjadhav2151@gmail.com>
2025-11-04 03:05:47 +0530

    looped

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,17 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
-         System.out.print("$ ");
 
         Scanner scanner = new Scanner(System.in);
-        String command = scanner.nextLine();
 
-        if(command.isEmpty()){
-            System.out.println("This is valid command");
-        }else{
-            System.out.println(command + ": command not found");
+        while(true){
+
+            System.out.print("$ ");
+            String command = scanner.nextLine();
+
+            if(command.isEmpty()){
+                System.out.println("This is valid command");
+            }else{
+                System.out.println(command + ": command not found");
+            }
         }
-
 
     }
 
